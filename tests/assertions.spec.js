9e46e906423bf7b9568286f526d391ece5ef8a32
IkhlasJihad <ikhlasjaydi@gmail.com>
2025-04-02 21:07:00 +0200

    assertions demo

---
+++ tests/assertions.spec.js
@@ -0,0 +1,32 @@
+// Assertions demo tests using website: https://kitchen.applitools.com/
+import { test, expect } from '@playwright/test';
+
+test('Assertions demo', async ({ page }) => {
+    await page.goto('https://kitchen.applitools.com/')
+    //1. Element existence assertions
+    await expect(page.locator('text=The Kitchen')).toHaveCount(1)
+    // ElementHandler if decision is needed
+    if(page.$('text=The Kitchen')) {
+        page.locator('text=The Kitchen').click()
+    }
+
+    //2. Element visibility assertions
+    await expect(page.locator('text=The Kitchen')).toBeVisible()
+    await expect.soft(page.locator('text=The Kitchen')).not.toBeHidden()// soft will not cause run stop upon assertion failure
+
+    //3. Element enabled assertions
+    await expect(page.locator('text=The Kitchen')).toBeEnabled()
+    await expect.soft(page.locator('text=The Kitchen')).not.toBeDisabled() 
+
+    //4. Element text
+    await expect(page.locator('text=The Kitchen')).toHaveText('The Kitchen')
+    await expect(page.locator('text=The Kitchen')).not.toHaveText('tada')
+
+    //5. Element Attribute
+    await expect(page.locator('text=The Kitchen')).toHaveAttribute('class', 'chakra-heading css-dpmy2a')
+    await expect(page.locator('text=The Kitchen')).toHaveAttribute('class', /.*css-dpmy2a/)
+
+    //await page.pause()  
+    //6. visual validation against a screenshot
+    await expect(page).toHaveScreenshot()
+})
